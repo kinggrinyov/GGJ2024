4304493107465e081e363c8c2126d157e5e78287
Ur3an <adso222@hotmail.com>
2024-01-26 21:49:26 +1100

    Stick to platform FIXED

--- GGJProject/Assets/Scripts/MovingPlatform.cs
+++ GGJProject/Assets/Scripts/MovingPlatform.cs
@@ -20,12 +20,12 @@ public class MovingPlatform : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.tag == "Player")
             collision.gameObject.transform.SetParent(transform);
     }
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.tag == "Player")
             collision.gameObject.transform.SetParent(null);
     }
 
