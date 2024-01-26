9d301dea11df9f50c7d51026546713a482b6edc9
unknown <ggj160@student.sae.edu.au>
2024-01-26 21:12:42 +1100

    bullet update

--- GGJProject/Assets/Scripts/Bullet.cs
+++ GGJProject/Assets/Scripts/Bullet.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    [SerializeField]
+    float damage = 7;
     private void OnTriggerEnter(Collider other)
     {
         //if player, deal damage
