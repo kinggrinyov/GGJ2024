22dd44c86b69019985d88b77af756068b21cc535
unknown <ggj162@student.sae.edu.au>
2024-01-26 20:18:44 +1100

    basic player left and right movement

--- GGJProject/Assets/Scripts/Player.cs
+++ GGJProject/Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody _rigidbody = null;
+
+    [field: SerializeField]
+    public float MovementSpeed { get; private set; }
+
+    void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float inputX = Input.GetAxis("Horizontal");
+
+        Vector3 originalVelocity = _rigidbody.velocity;
+
+        Vector3 movementDelta = new Vector3(MovementSpeed * inputX, originalVelocity.y, originalVelocity.z);
+
+        _rigidbody.velocity = movementDelta;
     }
 }
