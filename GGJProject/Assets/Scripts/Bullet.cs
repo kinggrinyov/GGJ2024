f66125130e35045979ff85f44f96a9605b498514
unknown <ggj160@student.sae.edu.au>
2024-01-26 21:42:15 +1100

    added timer on delete bullet

--- GGJProject/Assets/Scripts/Bullet.cs
+++ GGJProject/Assets/Scripts/Bullet.cs
@@ -7,10 +7,21 @@ public class Bullet : MonoBehaviour
 
     [SerializeField]
     float damage = 7;
+
+    [SerializeField]
+    private float BulletLifetime = 5;
+    private float timer = 0;
     private void OnTriggerEnter(Collider other)
     {
         //if player, deal damage
         Destroy(gameObject);
         
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= BulletLifetime)
+            Destroy(gameObject);
     }
 }
