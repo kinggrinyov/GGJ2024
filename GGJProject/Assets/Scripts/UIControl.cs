51515fc5af727e16469866fe778e369aa3949d98
unknown <ggj162@student.sae.edu.au>
2024-01-27 23:16:17 +1100

    Feel is good

--- GGJProject/Assets/Scripts/UIControl.cs
+++ GGJProject/Assets/Scripts/UIControl.cs
@@ -4,21 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class UIControl : MonoBehaviour
 {
-    [SerializeField]
-    private TextMeshProUGUI _loserText = null;
-
-    private void Start()
-    {
-        if(string.IsNullOrEmpty(GameInstance.Instance.LastPlayerNameLost))
-        {
-            _loserText.text = "";
-        }
-        else
-        {
-            _loserText.text = $"{GameInstance.Instance.LastPlayerNameLost} Has Lost, They Garb!";
-        }
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene("Game");       
